d0bda8f002c172bfc64cc8d7624f1f710ff8174d
rosaperez <rosaperezlarios@gmail.com>
2020-04-24 17:26:40 -0700

    working in emulator

--- App.js
+++ App.js
@@ -1,11 +1,7 @@
-// In App.js in a new project
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
-import * as React from 'react';
-import { View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-
-function HomeScreen() {
+export default function App() {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
@@ -13,16 +9,11 @@ function HomeScreen() {
   );
 }
 
-const Stack = createStackNavigator();
-
-function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
-
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
